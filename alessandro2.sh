346e1384b5a5e1ec20e789be3ac6bc0d58061720
Alessandro Fidelis <alessandro.fidelis@hotmail.com>
2018-06-17 21:01:15 -0300

    Update alessandro2.sh

--- alessandro2.sh
+++ alessandro2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-           py=0;		
-	   c=0
+           		
+	   
 	   sh=0
 	   total=0
 	
@@ -11,21 +11,13 @@
 	            sh=$(( $sh + 1 ))
 	            total=$(( $total + 1 ))
 	            ;;
-	         *.c)
-	            c=$(( $c + 1 ))
-	            total=$(( $total + 1 ))
-	            ;;
-	          *.py)
-	            py=$(( $py + 1 ))
-	            total=$(( $total + 1 ))
-	            ;;
 	      *)
 	      ;;  
 	      esac
 	   done
 	   echo "Total de arquivos = $total"
 	   echo "Total de arquivos .sh = $sh"
-	   echo "Total de arquivos .c = $c"
-	   echo "Total de arquivos .py = $py"
+
+
 
 
