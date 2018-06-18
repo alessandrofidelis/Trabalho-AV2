a8863715c1db626057be726c7a1737af057ab789
Alessandro Fidelis <alessandro.fidelis@hotmail.com>
2018-06-17 21:03:18 -0300

    Delete alessandro1.sh

--- alessandro1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git config --global user.name "Alessandro Fidelis"
-git config --global user.email alessandro.fidelis@hotmail.com 
- 
- git add * 
- git commit -m 'Trabalho AV2'
- 
- git push
