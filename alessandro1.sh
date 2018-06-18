f1fbc8b61ff3927583d772117f5aa71a5e4833ef
Alessandro Fidelis <alessandro.fidelis@hotmail.com>
2018-06-17 21:12:27 -0300

    Trabalho AV2

---
+++ alessandro1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git config --global user.name "Alessandro Fidelis"
+git config --global user.email alessandro.fidelis@hotmail.com 
+ 
+ git add * 
+ git commit -m 'Trabalho AV2'
+ 
+ git push
